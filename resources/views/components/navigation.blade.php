580a5ac8e1374049e50c7fa3bdec7591a30aaba0
ardanwungkul <ardanwungkul14@gmail.com>
2024-12-17 14:53:26 +0700

    first

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,60 @@
+<nav x-data="{ open: false }"
+    class="bg-watercouse-700 dark:bg-gray-800 border-y border-watercouse-600 dark:border-gray-700 shadow-lg bg-jar-wave-line fixed w-full top-0 z-50">
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
+        <div class="flex justify-between h-16">
+            <div class="shrink-0 flex items-center">
+                <a href="{{ route('welcome') }}" class="flex items-center gap-1">
+                    <div class="flex items-center gap-1">
+                        <img src="{{ asset('assets/images/logo-kab-paser.png') }}" class="h-16" alt="">
+                        <img src="{{ asset('assets/images/logo-apip.png') }}" class="h-16" alt="">
+                    </div>
+                    <div>
+                        <p class="font-semibold text-xl tracking-wide text-white"> Pemerintah Kabupaten Paser</p>
+                        <p class="font-medium text-sm text-gray-200">Inspektorat Daerah</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="flex items-center gap-5">
+                @if (Auth::user())
+                    <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
+                        class="py-1 text-white tracking-widest text-sm hover:border-b flex items-center gap-1"
+                        type="button">
+                        <p>Hii, {{ Auth::user()->name }}!! </p>
+                        <svg class="w-2.5 h-2.5 ms-3 fill-none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 4 4 4-4" />
+                        </svg>
+                    </button>
+
+                    <div id="dropdown"
+                        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow min-w-44 dark:bg-gray-700">
+                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                            aria-labelledby="dropdownDefaultButton">
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                this.closest('form').submit();"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                        <span class="ml-2 text-sm tracking-wide truncate">Logout</span>
+                                    </a>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                @else
+                    <a href="{{ route('login') }}"
+                        class="py-1 text-white tracking-widest text-sm hover:border-b">Login</a>
+                @endif
+            </div>
+
+
+        </div>
+    </div>
+
+
+</nav>
---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+
+<body class="font-poppins text-gray-900 antialiased">
+    @if (count($errors) > 0)
+        <div class="fixed bottom-5 left-5">
+            @foreach ($errors->all() as $error)
+                <div id="toast-error-{{ $loop->index }}"
+                    class="flex items-center gap-2 w-min p-4 text-gray-500 bg-white rounded-lg shadow border border-red-500"
+                    role="alert">
+                    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 bg-red-300 rounded-lg">
+                        <svg class="w-4 h-4 fill-red-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="-3.5 0 19 19">
+
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round">
+                            </g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path
+                                    d="M11.383 13.644A1.03 1.03 0 0 1 9.928 15.1L6 11.172 2.072 15.1a1.03 1.03 0 1 1-1.455-1.456l3.928-3.928L.617 5.79a1.03 1.03 0 1 1 1.455-1.456L6 8.261l3.928-3.928a1.03 1.03 0 0 1 1.455 1.456L7.455 9.716z">
+                                </path>
+                            </g>
+                        </svg>
+                        <span class="sr-only">Fire icon</span>
+                    </div>
+                    <div class="ms-3 text-sm font-normal whitespace-nowrap">{{ $error }}
+                    </div>
+                    <button type="button"
+                        class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                        data-dismiss-target="#toast-error-{{ $loop->index }}" aria-label="Close">
+                        <span class="sr-only">Close</span>
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                    </button>
+                </div>
+            @endforeach
+        </div>
+    @endif
+    @if (session('success'))
+        <div class="fixed bottom-5 left-5">
+            <div id="toast-success"
+                class="flex items-center gap-2 w-min p-4 text-gray-500 bg-white rounded-lg shadow border border-green-500"
+                role="alert">
+                <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 bg-green-300 rounded-lg">
+                    <svg viewBox="0 0 24 24" class="w-4 h-4 stroke-green-500 fill-none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round"></path>
+                    </svg>
+                    <span class="sr-only">Fire icon</span>
+                </div>
+                <div class="ms-3 text-sm font-normal whitespace-nowrap">{{ session('success') }}
+                </div>
+                <button type="button"
+                    class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8"
+                    data-dismiss-target="#toast-success" aria-label="Close">
+                    <span class="sr-only">Close</span>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    @endif
+    <div class="fixed bottom-5 right-5 z-20">
+        <button id="scrollToTop">
+            <svg class="svgIcon" viewBox="0 0 384 512">
+                <path
+                    d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z">
+                </path>
+            </svg>
+        </button>
+    </div>
+    <x-navigation></x-navigation>
+    <div class="pt-20 bg-white min-h-screen">
+        {{ $slot }}
+    </div>
+    <x-footer />
+</body>
+<script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.3.0/dist/dotlottie-wc.js" type="module"></script>
+<script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs" type="module"></script>
+<script>
+    const scrollToTopButton = document.getElementById('scrollToTop');
+
+    scrollToTopButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 100) {
+            scrollToTopButton.classList.add('show');
+        } else {
+            scrollToTopButton.classList.remove('show');
+        }
+    });
+</script>
+
+</html>
